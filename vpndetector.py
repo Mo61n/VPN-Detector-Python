90161ba5ac1b1a2f0ac0af0e90d21cada2ec7823
mo6in <108945415+Mo61n@users.noreply.github.com>
2024-02-29 15:31:59 +0330

    Update vpndetector.py

--- vpndetector.py
+++ vpndetector.py
@@ -1,12 +1,13 @@
 import requests
 import json
+import msvcrt
 
 def get_ip_address():
     data = str(requests.get('http://checkip.dyndns.com/').text)
     return data.split(': ')[-1].split('<')[0]
 
 def is_vpn(ip_address):
-    API_key = ""  # your API key 
+    API_key = ""  # your API key
     response = requests.get(f"https://vpnapi.io/api/{ip_address}?key={API_key}")
     data = json.loads(response.text)
     return data["security"]["vpn"]
@@ -20,3 +21,6 @@ if vpn_status:
     print("This IP address is using a VPN.")
 else:
     print("This IP address is not using a VPN.")
+
+print("Press any key to exit...")
+msvcrt.getch()
