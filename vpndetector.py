d7db226f15b2209afe4614d110dbc9095f89a83a
mo6in <108945415+Mo61n@users.noreply.github.com>
2024-03-15 10:33:58 +0330

    Update vpndetector.py

--- vpndetector.py
+++ vpndetector.py
@@ -1,26 +1,31 @@
 import requests
 import json
-import msvcrt
 
 def get_ip_address():
     data = str(requests.get('http://checkip.dyndns.com/').text)
     return data.split(': ')[-1].split('<')[0]
 
 def is_vpn(ip_address):
-    API_key = ""  # your API key
+    API_key = "2c1294b9924b42fe9eba83fcf032374d"  # API key (you can change it with yours )
     response = requests.get(f"https://vpnapi.io/api/{ip_address}?key={API_key}")
     data = json.loads(response.text)
     return data["security"]["vpn"]
 
-# Get IP address
-ip_address = get_ip_address()
-print(f"IP Address: {ip_address}")
+while True:
+    ip_address = get_ip_address()
+    print(f"IP Address: {ip_address}")
 
-vpn_status = is_vpn(ip_address)
-if vpn_status:
-    print("This IP address is using a VPN.")
-else:
-    print("This IP address is not using a VPN.")
+    vpn_status = is_vpn(ip_address)
+    if vpn_status:
+        print("This IP address is using a VPN. \U00002705")
+    else:
+        print("This IP address is not using a VPN. \U0000274C")
 
-print("Press any key to exit...")
-msvcrt.getch()
+    choice = input("Choose an option:\n1 - Check again \U0001F504\n2 - Close \U0001F44B\noption = ")
+
+    if choice == '1':
+        continue
+    elif choice == '2':
+        break
+    else:
+        print("Invalid choice. Please try again. \U000026A0") 
